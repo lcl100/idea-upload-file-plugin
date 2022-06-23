acd514d69e864faedc6abe02b57b2a039245687a
lck100 <2961316542@qq.com>
2022-06-23 21:56:07 +0800

    修复过时API问题

--- src/model/SSHSettingsState.java
+++ src/model/SSHSettingsState.java
@@ -1,10 +1,10 @@
 package model;
 
-import com.intellij.util.xmlb.XmlSerializerUtil;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,18 +15,20 @@ import org.jetbrains.annotations.Nullable;
 public class SSHSettingsState implements PersistentStateComponent<SSHSettingsState> {
 
     // 主机名或IP地址
-    public String host="";
+    public String host = "";
     // 端口号
-    public String port="22";
+    public String port = "22";
     // 用户名
-    public String username="";
+    public String username = "";
     // 登录密码
-    public String password="";
+    public String password = "";
     // 远程路径
-    public String remotePath="/tmp/";
+    public String remotePath = "/tmp/";
 
     public static SSHSettingsState getInstance() {
-        return ServiceManager.getService(SSHSettingsState.class);
+        // 下面的方法已经过时了
+//        return ServiceManager.getService(SSHSettingsState.class);
+        return ApplicationManager.getApplication().getService(SSHSettingsState.class);
     }
 
     @Nullable
